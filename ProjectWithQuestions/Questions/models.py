88d76fabea55b77f7a2f2e62882f8cd42797d6d2
Spaceocean <spaceocean@outlook.com>
2024-02-05 19:30:18 +0500

    almost finished project

--- ProjectWithQuestions/Questions/models.py
+++ ProjectWithQuestions/Questions/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    division = models.CharField(max_length=150, verbose_name="Дивизион")
+    company = models.CharField(max_length=150, verbose_name="Предприятие")
+    text = models.TextField(verbose_name='Вопрос')
+    email = models.EmailField(verbose_name='Электронная почта', blank=True)
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
--- ProjectWithQuestions/Questions/views.py
+++ ProjectWithQuestions/Questions/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Question
+from django.db.models import Count
 
-# Create your views here.
+
+def index(request):
+    q1 = Question.objects.all()
+    q2 = (Question.objects.values('division', 'company')
+          .annotate(company_count=Count('company')).order_by('-company_count'))
+    context = {'q1': q1, 'q2': q2}
+    if request.method == 'POST':
+        form = request.POST
+        division = form['select_division']
+        company = form['select_company']
+        if company == '':
+            return redirect('index')
+        if company == 'Другое':
+            company = form['user_company']
+        email = form['user_email']
+        text = form['user_question']
+        if division not in ["Топливный", "Машиностроительный", "ЯОК"] or text == '':
+            return redirect('index')
+        Question.objects.create(division=division, company=company, text=text, email=email).save()
+        return redirect('index')
+    return render(request, 'Questions/index.html', context=context)
